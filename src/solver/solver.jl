244ca92b46be38a041b6b6b495b065efc63d91b0
David Saidman <dsaidman@dsaidman-Desktop.localdomain>
2025-05-16 22:49:05 -0400

    leaving pluto

--- src/solver/solver.jl
+++ src/solver/solver.jl
@@ -1,10 +1,3 @@
-### A Pluto.jl notebook ###
-# v0.20.8
-
-using Markdown
-using InteractiveUtils
-
-# ╔═╡ 3aa97ecf-b438-427f-9ccd-a7d7e5f1d748
 module Definitions
 	
 export rowNames, columnNames, squares, neighbors, cellRows, cellColumns, puzzle0, families
@@ -56,7 +49,6 @@ puzzle0::Dict{String,String} = Dict(sq => "123456789" for sq in squares)
 
 end
 
-# ╔═╡ f82b8798-60d2-45e3-8880-74848c69261d
 function importPuzzle(filePath::String)::Dict{String,String}
 	puzzle = copy(Definitions.puzzle0)
 	open(filePath, "r") do file
@@ -68,17 +60,12 @@ function importPuzzle(filePath::String)::Dict{String,String}
 	return puzzle
 end
 
-
-# ╔═╡ 7127fda9-4ade-4719-aed4-be40c469d317
 isPuzzleComplete(pzl::Dict{String,String})::Bool = all([length(v) == 1 for v in values(pzl)])
 
-# ╔═╡ 640c4df3-49e3-4fa5-a8c8-088342123b73
 isFamilyCorrect(puzzle::Dict{String,String},familySquares::Vector{String})::Bool = join(sort(collect(join([puzzle[familySq] for familySq in familySquares],"")))) == "123456789"
 
-# ╔═╡ 08adcce5-9f36-4922-a219-5cc8227447c4
 isPuzzleSolved(pzl::Dict{String,String})::Bool = isPuzzleComplete(pzl) && all(map(fam -> isFamilyCorrect(pzl, fam), values(Definitions.families)))
 
-# ╔═╡ 9fc5601d-d091-4abb-9a86-c183aab57ffc
 # Is there a way to make this more julia-like? Probably
 function eliminationPass(puzzle::Dict{String,String})::Dict{String,String}
 	didChange::Bool = true
@@ -100,7 +87,6 @@ function eliminationPass(puzzle::Dict{String,String})::Dict{String,String}
 	return puzzle
 end
 
-# ╔═╡ d6b2d015-9ef4-43d1-be89-a38ccf96d3f3
 function getNextEntryPoint(puzzle::Dict{String,String})::String
 
 	nCounts = map(length,values(puzzle))
@@ -109,79 +95,22 @@ function getNextEntryPoint(puzzle::Dict{String,String})::String
 
 end
 
-# ╔═╡ 8f0ed96b-e3a4-4016-9f1f-e3a20d4ad676
+function solveTheThing(puzzle::Dict{String,Vector{String}})
 
-
-# ╔═╡ 703c49b8-d51c-401e-a0b8-1982d7b2d1cc
-puzzleFile = "C:\\Users\\david\\Projects\\sudokuSolver\\input\\mediumSample.ini"
-
-# ╔═╡ bfdaf6af-1bf5-40b2-aec4-ddcb4ddcfae0
-#function solveTheThing(puzzle::Dict{String,String})
-
-	# First do an elimination pass
-
-
-# ╔═╡ 757cd3a1-4715-4094-96c1-651d5aa4004b
-puzzle = importPuzzle(puzzleFile)
-
-# ╔═╡ 075a4274-5b27-466d-8226-2b68f7e5e2d2
-
-if ~isPuzzleSolved(puzzle)
+	if ~isPuzzleSolved(puzzle)
 		# Make a guess
 		puzzle    = eliminationPass(puzzle)
 		nextEntry = getNextEntryPoint(puzzle)
 		nextValues= puzzle[nextEntry]
-		#for nextValue in nextValues
-		nextValue = nextValues[1]
-		nextPuzzleGuess = copy(puzzle)
-		nextPuzzleGuess[nextEntry] = string(nextValue)
-        nextPuzzleGuess = eliminationPass(nextPuzzleGuess)
-	
-	
+		for nextValue in nextValues
+			nextValue = nextValues[1]
+			nextPuzzleGuess = copy(puzzle)
+			nextPuzzleGuess[nextEntry] = string(nextValue)
+			nextPuzzleGuess = eliminationPass(nextPuzzleGuess)
+		end
+
+	end
 end
 
-
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-Memoize = "c03570c3-d221-55d1-a50c-7939bbd78826"
-
-[compat]
-Memoize = "~0.4.4"
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
-julia_version = "1.11.5"
-manifest_format = "2.0"
-project_hash = "64f5358479b91887917e16ab0d9750f65a3d08e5"
-
-[[deps.MacroTools]]
-git-tree-sha1 = "1e0228a030642014fe5cfe68c2c0a818f9e3f522"
-uuid = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
-version = "0.5.16"
-
-[[deps.Memoize]]
-deps = ["MacroTools"]
-git-tree-sha1 = "2b1dfcba103de714d31c033b5dacc2e4a12c7caa"
-uuid = "c03570c3-d221-55d1-a50c-7939bbd78826"
-version = "0.4.4"
-"""
-
-# ╔═╡ Cell order:
-# ╠═3aa97ecf-b438-427f-9ccd-a7d7e5f1d748
-# ╠═f82b8798-60d2-45e3-8880-74848c69261d
-# ╠═7127fda9-4ade-4719-aed4-be40c469d317
-# ╠═640c4df3-49e3-4fa5-a8c8-088342123b73
-# ╠═08adcce5-9f36-4922-a219-5cc8227447c4
-# ╠═9fc5601d-d091-4abb-9a86-c183aab57ffc
-# ╠═d6b2d015-9ef4-43d1-be89-a38ccf96d3f3
-# ╠═8f0ed96b-e3a4-4016-9f1f-e3a20d4ad676
-# ╠═703c49b8-d51c-401e-a0b8-1982d7b2d1cc
-# ╠═757cd3a1-4715-4094-96c1-651d5aa4004b
-# ╠═bfdaf6af-1bf5-40b2-aec4-ddcb4ddcfae0
-# ╠═075a4274-5b27-466d-8226-2b68f7e5e2d2
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
+puzzleFile = "C:\\Users\\david\\Projects\\sudokuSolver\\input\\mediumSample.ini"
+puzzle = importPuzzle(puzzleFile)
