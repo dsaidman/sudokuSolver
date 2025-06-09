ff53664b056a13cde44c772c54a6cc20201df905
David Saidman <dsaidman@dsaidman-Desktop.localdomain>
2025-06-09 19:16:41 -0400

    working with sets

--- src/solver/JSolver.jl
+++ src/solver/JSolver.jl
@@ -86,43 +86,38 @@ end
 
 
 # Could use StatsBase, kinda like histcounts to get how often each value appears
-@inline function countOccurances(puzzle::SudokuPuzzleT)::Dict{Int,Int}
+@inline function countAllSquareOccurances(puzzle::SudokuPuzzleT)::Dict{Int,Int}
 	# Collect all values in the puzzle together
-    return Dict(val => count(==(val), values(puzzle)) for val in squareValue0)
+    allPuzzleValues = reduce(vcat,collect.(values(puzzle)))
+    return Dict(val => count(==(val), allPuzzleValues) for val in collect(squareValue0))
     
 end
 
 function getNextEntryPoint(puzzle::SudokuPuzzleT)
 
 	# Of all unknowns, find the unknown value that occurs most often
-	unsolvedCount             = countOccurances(puzzle)
+    unsolvedCount = countAllSquareOccurances(puzzle)
 
     # find most frequently unsolved number (occurs most in incomplete squares)
     mostFrequentUnsolved = findfirst(v -> v == maximum(values(unsolvedCount)), unsolvedCount)
 
 	# get the square with fewest possiblities that contains mostFrequentlyUnresolved
 	smallestMostFrequent  = [length(v) for (k,v) in puzzle if length(v) > 1 && mostFrequentUnsolved in v]
-    if isempty(smallestMostFrequent)
-		return false   
-	else
-        smallestMostFrequent = minimum(smallestMostFrequent) # Return if no choices
-	end
-
+    smallestMostFrequent  = length(smallestMostFrequent) > 0 ? minimum(smallestMostFrequent) : return false
+    
 	# With the value that occurs most often (mostFrequentUnsolved), find the square 
 	# with mostFrequentUnsolved with fewest remaining possible values (smallestMostFrequent). 
 	# The selection will eliminate the most possible paths
     nextSquareChoice = squares[
-		findfirst(k -> length(puzzle[k]) == smallestMostFrequent && mostFrequentUnsolved in puzzle[k], squares)
-		]
+        findfirst(k -> length(puzzle[k]) == smallestMostFrequent && mostFrequentUnsolved in puzzle[k], squares)
+    ]
 	return length(puzzle[nextSquareChoice]) <= 1 ? false : nextSquareChoice
 
 end
 
 
-
 function solve(puzzle::SudokuPuzzleT)
     # Make sure the puzzle is valid
-
     function solveTheThing(puzzle::SudokuPuzzleT)
 
         isPuzzleSolved(puzzle) && return puzzle
@@ -131,10 +126,10 @@ function solve(puzzle::SudokuPuzzleT)
         puzzle = eliminationPass(puzzle)
 
         isPuzzleSolved(puzzle) && return puzzle
+        !allFamiliesValid(puzzle) && return false
         isPuzzleComplete(puzzle) && return false
         nextEntry = getNextEntryPoint(puzzle)
         nextEntry == false && return false
-        
         nextValues = sort(
             collect(puzzle[nextEntry]), 
             by=x -> count(==(x), reduce(vcat,collect.(values(puzzle)))), 
@@ -145,6 +140,7 @@ function solve(puzzle::SudokuPuzzleT)
         for nextValue in nextValues
             numRecursions += 1
             nextPuzzleGuess = deepcopy(puzzle)
+            
             !allFamiliesValid(nextPuzzleGuess) && continue
             nextPuzzleGuess[nextEntry] = Set(nextValue)
             nextPuzzleGuess = solveTheThing(nextPuzzleGuess)
@@ -204,7 +200,7 @@ using ..JSolver
 @inline function testIt()
     #puzzleStr = ".15.7....4..8..75...8..9.169641.7.3..8239.5..5....4.9..2.41.8....17.39.4...92..65"
 
-    puzzleStr = ".......7..1.9.4..8..9........5..17....3..96..1...67..9........4.82.46...3...8...."
+    puzzleStr = "..5...7....38.1..5..2.6..399...8...7.....5....7.2..14...........8613............3"
     puzzle = Dict( 
         squares[ix] => v == '.' ? Set([1,2,3,4,5,6,7,8,9]) :  Set(Int(v)-Int('0')) for (ix, v) in enumerate(puzzleStr)
         )
